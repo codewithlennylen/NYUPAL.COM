a75b8a592a700c43cd621edccc052c579a6f23a2
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2021-06-28 17:33:52 +0300

    Index Page -> Images from DB :+1:
    Dummy data generator had a bug that added non-existent images in the DB.

--- codebase/app/views.py
+++ codebase/app/views.py
@@ -8,4 +8,16 @@ main_view = Blueprint('main_view', __name__)
 @main_view.route('/')
 def index():
     propertys = Property.query.all()
-    return render_template("index.html",propertys=propertys)
+
+    propertyImages = []
+    for property in propertys:
+        # Getting the image dirs
+        images = property.property_images.split('|')
+        profilePic = images[0] # get the primary image (property_profile_pic)
+        propertyImages.append(profilePic) 
+
+    print(propertyImages)
+
+    return render_template("index.html",
+                           propertys=propertys,
+                           propertyImages=propertyImages)
