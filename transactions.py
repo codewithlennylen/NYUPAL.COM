e4d05bcc5e84882adc725ecdec3e70228fce83e7
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2022-03-08 13:24:59 +0300

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -2,13 +2,15 @@ from app.models import PaymentGateway
 from app import create_app, db
 
 
-transact_id = 'ABC123'
+transact_id = ['x79FqQ','w9Q0MA','pM6aAg',
+                'op8Axg','fXD9Rg','qS1Xuw']
 
 with create_app().app_context():
-    p = PaymentGateway(
-        transaction_id = transact_id
-    )
+    for transact in transact_id:
+        p = PaymentGateway(
+            transaction_id = transact.upper()
+        )
 
-    db.session.add(p)
-    db.session.commit()
-    print(f"Added Transaction: {transact_id}")
+        db.session.add(p)
+        db.session.commit()
+        print(f"Added Transaction: {transact.upper()}")
